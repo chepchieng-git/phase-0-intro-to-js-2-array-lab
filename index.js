702fcb8fd09b5875bd8c77733e28b0778696d90c
Wycliffe <wycliffekiprono4@gmail.com>
2023-03-11 14:14:08 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    
+    cats.push("Ralph");
+    return destructivelyAppendCat;
+
+}
+
+function destructivelyPrependCat(name) {
+    
+    cats.unshift("Bob")
+    return destructivelyPrependCat;
+} 
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+    return destructivelyRemoveLastCat;
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+    return destructivelyRemoveFirstCat;
+}
+
+function appendCat(name) {
+    const appendCat = [...cats, "Broom"]
+    return appendCat;
+}
+
+function prependCat(name) {
+    const prependCat = ["Arnold", ...cats];
+    return prependCat;
+}
+
+function removeLastCat(name) {
+    const removeLastCat = cats.slice(0, cats.length -1);
+    return removeLastCat;
+}
+function removeFirstCat(name) {
+    const removeFirstCat = cats.slice(1);
+    return removeFirstCat;
+}
